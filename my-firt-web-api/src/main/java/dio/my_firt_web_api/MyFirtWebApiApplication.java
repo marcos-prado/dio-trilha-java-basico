6e51d88f9f1da3746dbbf3998c70bf177109528a
Marcos <marcosizaiasprado@gmail.com>
2025-04-18 20:52:45 -0300

    RestController

---
+++ my-firt-web-api/src/main/java/dio/my_firt_web_api/MyFirtWebApiApplication.java
@@ -0,0 +1,13 @@
+package dio.my_firt_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirtWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirtWebApiApplication.class, args);
+	}
+
+}
---
+++ my-firt-web-api/src/test/java/dio/my_firt_web_api/MyFirtWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.my_firt_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirtWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
